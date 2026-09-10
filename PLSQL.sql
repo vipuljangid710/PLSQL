d166bd36c095832e8fe0cf1f9c34afe6d07f0024
vipuljangid710 <114286601+vipuljangid710@users.noreply.github.com>
2026-09-10 13:14:55 +0530

    Add files via upload

---
+++ PLSQL.sql
@@ -0,0 +1,22 @@
+
+
+
+BEGIN
+    
+
+
+--     dbms_output.PUT_LINE('hello vipul');
+--      dbms_output.PUT_LINE('hello raj');
+--       dbms_output.PUT_LINE(age);
+--       age:=20;
+--         dbms_output.PUT_LINE(age);
+
+-- if(age=20) THEN
+
+--         dbms_output.PUT_LINE('we got 20');
+-- else
+--     dbms_output.PUT_LINE('not 20');
+-- end if;
+
+
+END;
